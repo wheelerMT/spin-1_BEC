2ed8c34d6354c9a86a15418693b4654d9c0f6c5c
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2020-10-21 15:09:46 +0100

    Fixed memory issues where entire datasets were being loaded in at once rather than each frame sequentially. Fixed bug in naming correlations file.

--- diagnostics/correlations.py
+++ diagnostics/correlations.py
@@ -32,7 +32,7 @@ integral_phase = np.empty((Nx, Ny, num_of_frames - frame_start))
 for i in range(frame_start, num_of_frames):
     alpha_perp = np.fft.fft2(-2 * psi_plus[:, :, i] * psi_minus[:, :, i])
     integral_phase[:, :, i - frame_start] = np.fft.fftshift(1 / (n_0 * Nx) ** 2 * np.fft.ifft2(alpha_perp * np.conj(alpha_perp))).real
-    integral_phase = np.where(integral_phase < 0, 0, integral_phase)
+    integral_phase[:, :, i] = np.where(integral_phase[:, :, i] < 0, 0, integral_phase[:, :, i])
 
 # Calculate nematic OP:
 integral_nematic = np.empty((Nx, Ny, num_of_frames))
@@ -40,7 +40,7 @@ for i in range(frame_start, num_of_frames):
     q_xx_k = np.fft.fft2(np.real(np.conj(psi_plus[:, :, i]) * psi_minus[:, :, i]))
     q_xy_k = np.fft.fft2(np.imag(np.conj(psi_plus[:, :, i]) * psi_minus[:, :, i]))
     integral_nematic[:, :, i - frame_start] = np.fft.fftshift(4 / (n_0 * Nx) ** 2 * np.fft.ifft2(q_xx_k * np.conj(q_xx_k) + q_xy_k * np.conj(q_xy_k))).real
-    integral_nematic = np.where(integral_nematic < 0, 0, integral_nematic)
+    integral_nematic[:, :, i] = np.where(integral_nematic[:, :, i] < 0, 0, integral_nematic[:, :, i])
 
 data_file.close()
 
@@ -49,7 +49,7 @@ centerx = Nx // 2
 centery = Ny // 2
 
 # Generate data file:
-corr_file = '{}_corr.hdf5'.format(filename)
+corr_file = '{}_corr'.format(filename)
 corr_data = h5py.File('../data/{}.hdf5'.format(corr_file), 'w')
 g_theta = corr_data.create_dataset('g_theta', (box_radius, num_of_frames - frame_start))
 g_phi = corr_data.create_dataset('g_phi', (box_radius, num_of_frames - frame_start))
