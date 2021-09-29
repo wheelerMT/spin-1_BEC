a8d6dda28d8d909064db6d6d44fafaf31ebdccd5
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-09-29 11:19:58 +0100

    Added extra Skyrmion angle contour plot.

--- diagnostics/plots/kibble-zurek/1d_spins.py
+++ diagnostics/plots/kibble-zurek/1d_spins.py
@@ -6,7 +6,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 # Load in data:
-filename = input('Enter data filename: ')
+filename = '1d_kibble-zurek/1d_polar-BA-FM_750'  # input('Enter data filename: ')
 data_file = h5py.File('../../../data/{}.hdf5'.format(filename), 'r')
 
 psi_plus = data_file['wavefunction/psi_plus']
@@ -33,14 +33,16 @@ F_perp = fx + 1j * fy
 spacetime_F = np.empty((len(X), num_of_frames))
 spacetime_F_perp = np.empty((len(X), num_of_frames))
 spacetime_Fz = np.empty((len(X), num_of_frames))
+spacetime_theta = np.empty((len(X), num_of_frames))
 
 for i in range(num_of_frames):
     spacetime_F[:, i] = F[:, i]
     spacetime_F_perp[:, i] = np.angle(F_perp)[:, i]
     spacetime_Fz[:, i] = fz[:, i]
+    spacetime_theta[:, i] = np.arccos(fx[:, i].real)
 
 # Set up plots
-fig, ax = plt.subplots(3, 1, sharex=True)
+fig, ax = plt.subplots(4, 1, sharex=True)
 for axis in ax:
     axis.set_ylabel(r'$x/\xi_s$')
     if axis == ax[0]:
@@ -48,20 +50,26 @@ for axis in ax:
     if axis == ax[1]:
         axis.set_title(r'Angle$(F_{perp})$')
     if axis == ax[2]:
+        axis.set_title(r'$\theta$')
+    if axis == ax[3]:
         axis.set_title(r'$F_z$')
         axis.set_xlabel(r'$t/\tau$')
 
 # Plot the results
 F_plot = ax[0].pcolormesh(time, X, spacetime_F, vmin=0, vmax=1, shading='auto', cmap='jet')
 F_perp_plot = ax[1].pcolormesh(time, X, spacetime_F_perp, vmin=-np.pi, vmax=np.pi, shading='auto')
-Fz_plot = ax[2].pcolormesh(time, X, spacetime_Fz, vmin=-1, vmax=1, shading='auto')
+theta_plot = ax[2].pcolormesh(time, X, spacetime_theta, vmin=-np.pi, vmax=np.pi, shading='auto')
+Fz_plot = ax[3].pcolormesh(time, X, spacetime_Fz, vmin=-1, vmax=1, shading='auto')
 
 # Set up cbars
 F_cbar = plt.colorbar(F_plot, ax=ax[0])
 F_perp_cbar = plt.colorbar(F_perp_plot, ax=ax[1])
 F_perp_cbar.set_ticks([-np.pi, 0, np.pi])
 F_perp_cbar.set_ticklabels([r'-$\pi$', '0', r'$\pi$'])
-Fz_cbar = plt.colorbar(Fz_plot, ax=ax[2])
+theta_cbar = plt.colorbar(theta_plot, ax=ax[2])
+theta_cbar.set_ticks([-np.pi, 0, np.pi])
+theta_cbar.set_ticklabels([r'-$\pi$', '0', r'$\pi$'])
+Fz_cbar = plt.colorbar(Fz_plot, ax=ax[3])
 Fz_cbar.set_ticks([-1, 1])
 Fz_cbar.set_ticklabels(['-1', '1'])
 
