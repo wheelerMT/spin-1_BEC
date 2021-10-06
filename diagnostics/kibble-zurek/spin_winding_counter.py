4ae016324747dfa9b80cc0047563b792bad7e24d
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-10-06 09:52:40 +0100

    Updated filepath.

--- diagnostics/kibble-zurek/spin_winding_counter.py
+++ diagnostics/kibble-zurek/spin_winding_counter.py
@@ -14,7 +14,7 @@ quench_rates = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700,
 spin_winding_list = []
 
 for quench in quench_rates:
-    filename = '../../data/1d_kibble-zurek/1d_polar-BA-FM_{}.hdf5'.format(quench)
+    filename = '../../data/1d_kibble-zurek/single_runs/1d_polar-BA-FM_{}.hdf5'.format(quench)
     with h5py.File(filename, 'r') as data_file:
         # Load in data:
         x = data_file['grid/x']
