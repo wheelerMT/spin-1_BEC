6049ee86b862bbbe4624fd4e255ddc4ebdfd4969
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-11-17 11:03:57 +0000

    Updated h5py file accessors

--- diagnostics/plots/kibble-zurek/FM_domains_vs_quench_rate.py
+++ diagnostics/plots/kibble-zurek/FM_domains_vs_quench_rate.py
@@ -6,7 +6,7 @@ matplotlib.use('TkAgg')
 
 # Load in diagnostics data
 diag_file = h5py.File('../../../data/1d_kibble-zurek/diagnostics/1d_polar-BA-FM_diag.hdf5', 'r')
-quench_rates = diag_file['quench_rates'][...]
+quench_rates = diag_file['quench_times'][...]
 fm_domains = diag_file['FM_domains/average'][...]
 
 # Calculate standard deviation for each quench:
@@ -21,8 +21,8 @@ ax.set_yscale("log")
 ax.errorbar(quench_rates, fm_domains, yerr=fm_domains_sd, capsize=5, ecolor='k', fmt='none')
 ax.loglog(quench_rates, fm_domains, 'ko')
 ax.loglog(quench_rates[1:], 1.9e3 * quench_rates[1:] ** (-1 / 3), 'k--', alpha=0.2, label=r'$\tau_Q^{-1/3}$')
-ax.loglog(quench_rates[:], 1.15e3 * quench_rates[:] ** (-1 / 4), 'k:', label=r'$\tau_Q^{-1/4}$')
-ax.set_xlim(0.8e2, 1e3)
+ax.loglog(quench_rates[:], 0.87e3 * quench_rates[:] ** (-1 / 4), 'k:', label=r'$\tau_Q^{-1/4}$')
+# ax.set_xlim(0.8e2, 1e3)
 ax.set_ylabel(r'$N_d$')
 ax.set_xlabel(r'$\tau_Q$')
 ax.legend()
