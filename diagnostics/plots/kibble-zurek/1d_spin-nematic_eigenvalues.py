1cb14e37d2e13f5c070345aff4724e33ba59b90f
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-16 07:57:26 +0000

    Switched to Fourier space calculation of tensor product

--- diagnostics/plots/kibble-zurek/1d_spin-nematic_eigenvalues.py
+++ diagnostics/plots/kibble-zurek/1d_spin-nematic_eigenvalues.py
@@ -2,6 +2,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import conj
+from numpy.fft import fft, ifft, ifftshift
 
 # Load in data
 filename_prefix = '1d_polar-BA-FM_5000'
@@ -35,46 +36,51 @@ n_0 = abs(psi_0) ** 2
 n_minus = abs(psi_minus) ** 2
 n = abs(psi_plus) ** 2 + abs(psi_0) ** 2 + abs(psi_minus) ** 2
 
-Q_xx = np.real(conj(psi_plus) * psi_minus) \
-       - 0.5 * (n_plus + n_minus) + n / 3
-Q_yy = -np.real(conj(psi_plus) * psi_minus) - 0.5 * (n_plus + n_minus) + n / 3
-Q_zz = -n_0 + n / 3
-Q_xy = np.imag(conj(psi_plus) * psi_minus)
-Q_xz = -np.sqrt(2.) / 4 * (psi_0 * (conj(psi_minus - psi_minus))
-                           + conj(psi_0) * (psi_minus - psi_plus))
-Q_yz = -1j * np.sqrt(2.) / 4 * (psi_0 * (conj(psi_minus + psi_minus))
-                                - conj(psi_0) * (psi_minus + psi_plus))
-
-Q_xx0 = np.roll(Q_xx, Nx // 3)
-Q_yy0 = np.roll(Q_yy, Nx // 3)
-Q_zz0 = np.roll(Q_zz, Nx // 3)
-Q_xy0 = np.roll(Q_xy, Nx // 3)
-Q_xz0 = np.roll(Q_xz, Nx // 3)
-Q_yz0 = np.roll(Q_yz, Nx // 3)
+Q_xx = fft(np.real(conj(psi_plus) * psi_minus) - 0.5 * (n_plus + n_minus) + n / 3)
+Q_yy = fft(-np.real(conj(psi_plus) * psi_minus) - 0.5 * (n_plus + n_minus) + n / 3)
+Q_zz = fft(-n_0 + n / 3)
+Q_xy = fft(np.imag(conj(psi_plus) * psi_minus))
+Q_xz = fft(-np.sqrt(2.) / 4 * (psi_0 * (conj(psi_minus - psi_minus)) + conj(psi_0) * (psi_minus - psi_plus)))
+Q_yz = fft(-1j * np.sqrt(2.) / 4 * (psi_0 * (conj(psi_minus + psi_minus)) - conj(psi_0) * (psi_minus + psi_plus)))
 
 # Calculate Q_tilde
-Qt_xx = Q_xx0 * Q_xx + Q_xy0 * Q_xy + Q_xz0 * Q_xz
-Qt_xy = Q_xx0 * Q_xy + Q_xy0 * Q_yy + Q_xz0 * Q_yz
-Qt_xz = Q_xx0 * Q_xz + Q_xy0 * Q_yz + Q_xz0 * Q_zz
+Qt_xx = (1 / Nx * ifftshift(ifft(Q_xx * conj(Q_xx) + Q_xy * conj(Q_xy) + Q_xz * conj(Q_xz)))).real
+Qt_xy = (1 / Nx * ifftshift(ifft(Q_xx * conj(Q_xy) + Q_xy * conj(Q_yy) + Q_xz * conj(Q_yz)))).real
+Qt_xz = (1 / Nx * ifftshift(ifft(Q_xx * conj(Q_xz) + Q_xy * conj(Q_yz) + Q_xz * conj(Q_zz)))).real
 
-Qt_yx = Q_xy0 * Q_xx + Q_yy0 * Q_xy + Q_yz0 * Q_xz
-Qt_yy = Q_xy0 * Q_xy + Q_yy0 * Q_yy + Q_yz0 * Q_yz
-Qt_yz = Q_xy0 * Q_xz + Q_yy0 * Q_yz + Q_yz0 * Q_zz
+Qt_yx = (1 / Nx * ifftshift(ifft(Q_xy * conj(Q_xx) + Q_yy * conj(Q_xy) + Q_yz * conj(Q_xz)))).real
+Qt_yy = (1 / Nx * ifftshift(ifft(Q_xy * conj(Q_xy) + Q_yy * conj(Q_yy) + Q_yz * conj(Q_yz)))).real
+Qt_yz = (1 / Nx * ifftshift(ifft(Q_xy * conj(Q_xz) + Q_yy * conj(Q_yz) + Q_yz * conj(Q_zz)))).real
 
-Qt_zx = Q_xz0 * Q_xx + Q_yz0 * Q_xy + Q_zz0 * Q_xz
-Qt_zy = Q_xz0 * Q_xy + Q_yz0 * Q_yy + Q_zz0 * Q_yz
-Qt_zz = Q_xz0 * Q_xz + Q_yz0 * Q_yz + Q_zz0 * Q_zz
+Qt_zx = (1 / Nx * ifftshift(ifft(Q_xz * conj(Q_xx) + Q_yz * conj(Q_xy) + Q_zz * conj(Q_xz)))).real
+Qt_zy = (1 / Nx * ifftshift(ifft(Q_xz * conj(Q_xy) + Q_yz * conj(Q_yy) + Q_zz * conj(Q_yz)))).real
+Qt_zz = (1 / Nx * ifftshift(ifft(Q_xz * conj(Q_xz) + Q_yz * conj(Q_yz) + Q_zz * conj(Q_zz)))).real
 
-eigenvalues = []
+eigenvalues_1 = []
+eigenvalues_2 = []
+eigenvalues_3 = []
+eigenvector_1 = []
+eigenvector_2 = []
+eigenvector_3 = []
 for index in range(Nx):
     Qt = np.matrix([[Qt_xx[index], Qt_xy[index], Qt_xz[index]],
                     [Qt_yx[index], Qt_yy[index], Qt_yz[index]],
                     [Qt_zx[index], Qt_zy[index], Qt_zz[index]]])
     w, v = np.linalg.eig(Qt)
 
-    eigenvalues.append(w[0])
+    eigenvalues_1.append(w[0])
+    eigenvector_1.append(v[0])
+    eigenvalues_2.append(w[1])
+    eigenvector_2.append(v[1])
+    eigenvalues_3.append(w[2])
+    eigenvector_3.append(v[2])
 
 # plt.plot(x[-Nx // 2 - 100:Nx // 2 + 100], eigenvalues[-Nx // 2 - 100:Nx // 2 + 100], 'ko')
-plt.plot(x, eigenvalues, 'ko')
-plt.savefig(f'../../../../plots/spin-1/{filename_prefix}_eigenvalue.png', bbox_inches='tight')
+plt.plot(x, eigenvalues_1, 'ko')
+plt.plot(x, eigenvalues_2, 'ro')
+plt.plot(x, eigenvalues_3, 'bo')
+
+# print(eigenvector_1[np.where(np.array(eigenvalues_1) > 0.1)[0][0]])
+# plt.savefig(f'../../../../plots/spin-1/{filename_prefix}_eigenvalue.png', bbox_inches='tight')
+plt.ylim(0, 0.3)
 plt.show()
