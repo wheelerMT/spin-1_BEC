dc1fa25f0a763cceaf63de65a2d4562706e71cea
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-08-15 09:55:52 +0100

    Re-wrote code to save diagnostics data to a file

--- diagnostics/kibble-zurek/FM_domain_counter.py
+++ diagnostics/kibble-zurek/FM_domain_counter.py
@@ -1,26 +1,38 @@
 import h5py
 import numpy as np
 
-# * We want to count domains by measuring the number of density peaks
-# * After the number of domains has been stabilised
+quenches = [i for i in range(500, 8500, 500)]
+runs = [i for i in range(1, 51)]
+
+# Load in diagnostics file
+quench_type = '1d_BA-FM'
+diag_file = h5py.File(f'../../scratch/data/spin-1/kibble-zurek/diagnostics/{quench_type}_domains.hdf5', 'w')
 
 # Load in data:
-tau_q = 1000
-domain_count_plus = 0
-domain_count_minus = 0
-with h5py.File('../../scratch/data/spin-1/kibble-zurek/1d_polar-BA-FM_{}.hdf5'.format(tau_q), 'r') as data_file:
-    psi_plus = data_file['wavefunction/psi_plus'][:, -1]
-    psi_minus = data_file['wavefunction/psi_minus'][:, -1]
-    peaks_plus = np.where(abs(psi_plus) ** 2 > 0.75)
-    peaks_minus = np.where(abs(psi_minus) ** 2 > 0.75)
+for quench in quenches:
+    nd_plus = []
+    nd_minus = []
+    for run in runs:
+        domain_count_plus = 0
+        domain_count_minus = 0
+        with h5py.File(f'../../scratch/data/spin-1/kibble-zurek/ensembles/tau_q={quench}/{quench_type}_{run}',
+                       'r') as data_file:
+            psi_plus = data_file['wavefunction/psi_plus'][:, -1]
+            psi_minus = data_file['wavefunction/psi_minus'][:, -1]
+            peaks_plus = np.where(abs(psi_plus) ** 2 > 0.75)
+            peaks_minus = np.where(abs(psi_minus) ** 2 > 0.75)
 
-    for i in range(np.size(peaks_plus) - 1):
-        if abs(peaks_plus[0][i] - peaks_plus[0][i + 1]) > 2:
-            domain_count_plus += 1
-    for i in range(np.size(peaks_minus) - 1):
-        if abs(peaks_minus[0][i] - peaks_minus[0][i + 1]) > 2:
-            domain_count_minus += 1
+            for i in range(np.size(peaks_plus) - 1):
+                if abs(peaks_plus[0][i] - peaks_plus[0][i + 1]) > 2:
+                    domain_count_plus += 1
+            for i in range(np.size(peaks_minus) - 1):
+                if abs(peaks_minus[0][i] - peaks_minus[0][i + 1]) > 2:
+                    domain_count_minus += 1
 
-print(f'For tau_q = {tau_q}:')
-print(f'Plus domains: {domain_count_plus}, minus domains: {domain_count_minus}, '
-      f'total domains: {domain_count_plus + domain_count_minus}')
+            nd_plus.append(domain_count_plus)
+            nd_minus.append(domain_count_minus)
+
+    # Save to dataset
+    diag_file.create_dataset(f'{quench}/nd_plus', data=nd_plus)
+    diag_file.create_dataset(f'{quench}/nd_minus', data=nd_minus)
+    diag_file.create_dataset(f'{quench}/nd_total', data=[sum(x) for x in zip(nd_plus, nd_minus)])
